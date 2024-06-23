c9542dfb52764078aae8d7daf7b08e25f85f0f6b
mvaibhav06 <55031067+mvaibhav06@users.noreply.github.com>
2024-06-23 09:20:57 -0500

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,28 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        long start = 1;
+        long end = n;
+
+        while(start <= end){
+            long mid = (start+end)/2;
+            int num = guess((int) mid);
+            if(num == 0){
+                return (int) mid;
+            }else if(num == -1){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+}
